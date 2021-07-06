55270f50b2dc3a66ce5caaeb3977878e30e06122
mari <maromarak@gmail.com>
2021-07-06 10:18:43 +0200

    Shopping cart: part 1

--- shoppingcart/cart.php
+++ shoppingcart/cart.php
@@ -1,5 +1,36 @@
 <?php
 
 // A kosár oldal, amely tartalmazza az összes terméket, amelyeket a felhasználó hozzáadott. Láthatjuk a végösszeget is.
+// Ha a felh. ráklikkel a "Kosárba" gombra a "Termékek" oldalon, akkor ellenőrízhetjük a form adatokat
 
+if(isset($_POST['product_id'], $_POST['quantity'] && is_numeric($_POST['product_id']) && is_numeric($_POST['quantity'])) {
+    // Beállítjuk a POST változókat, hogy könnyedén azonosítsuk őket, muszáj integereknek lenniük
+    $product_id = (int)$_POST['product_id'];
+    $quantity = (int)$_POST['quantity'];
+    // Előkészítjük az SQL lekérdezést, igazából megnézzük, hogy létezik-e a termék a DB-ben
+    $stmt = $pdo->prepare('SELECT * from products WHERE id =?');
+    $stmt->execute($_POST['product_id']);
+    // Fetcheljük a terméket a DB-ből és tömblént visszaadjuk
+    $product=$stmt->fetch(PDO::FETCH_ASSOC);
+    // Megnézzük, hogy nem-e üres a tömb (A termék létezik)
+    if($product && $quantity > 0) {
+        // A termék létezik a DB-ben, most frissíthetjük a session változót a kosárban
+        if(isset($_SESSION['cart']) && is_array($_SESSION['cart'])) {
+            if(array_key_exists($product_id && $_SESSION['cart'])) {
+                // A termék létezik a kosárban, szóval csak frissíteni kell a mennyiséget
+                $_SESSION['cart'][$product_id] += $quantity;
+            } else {
+                // A termék nincs a kosrában, ezért hozzáadjuk
+                $_SESSION['cart'][$product_id] = $quantity;
+            }
+        } else {
+            // Nincs termék a kosrában, ezért hozzáadjuk az első terméket a kosárba
+            $_SESSION['cart'] = array($product_id => $quantity);
+
+        }
+    }
+    // A form újboli beküldésének a megakadályozása
+    header('location: index.php?page=cart');
+    exit;
+}
 ?>
